07b2b61ad86b306ce5c696bb11c12ba0558944e6
ipinid613 <ipinid613@kakao.com>
2021-08-11 19:33:02 +0900

    로그아웃

--- ch04/logout.php
+++ ch04/logout.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
-    unset($_SESSION["username"]);
-    setcookie("username","");
-    setcookie("password","");
+    unset($_SESSION["username"]); // 세션 삭제
+    setcookie("username",""); // 쿠키 삭제
+    setcookie("password",""); // 쿠키 삭제
     header("Location:http://localhost:63342/htdocs/ch04/login_form.php");
     ?>
